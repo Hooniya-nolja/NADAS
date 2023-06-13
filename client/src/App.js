f505bcb151b54a80bbc7c8c25818976dfb8a0b11
Hooniya-nolja <wkdckdgns1017@gmail.com>
2023-06-13 17:16:40 +0900

    [기능] : 엑셀 업로드 기능 및 결과표 출력

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import * as XLSX from "xlsx";
 
 function App() {
+  const [items, setItems] = useState([]);
 
   const readExcel=(file)=>{
 
     const promise = new Promise((resolve,reject)=>{
-
       const fileReader = new FileReader();
-      fileReader.readAsArra
-      
-    })
+      fileReader.readAsArrayBuffer(file);
 
-  }
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb=XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d)=>{
+      setItems(d);
+    });
+  };
 
   return (
     <div>
@@ -23,6 +45,29 @@ function App() {
           readExcel(file);
         }}
       />
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Keyword</th>
+            <th scope="col">Category</th>
+            <th scope="col">BlockRank</th>
+            <th scope="col">Site</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            items.map((d) => (
+              <tr key={d.Keyword}>
+                <th>{d.Keyword}</th>
+                <td>{d.Category}</td>
+                <td>{d.BlockRank}</td>
+                <td>{d.Site}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
